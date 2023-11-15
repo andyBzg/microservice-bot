dcf0cc222f4190022effbeb1ebe78a2a3c671ad1
andyBzg <andr.bzg@gmail.com>
2023-11-15 11:21:29 +0100

    Added pom-files for project modules Added app Main class

--- dispatcher/src/main/java/com/andybzg/DispatcherApplication.java
+++ dispatcher/src/main/java/com/andybzg/DispatcherApplication.java
@@ -1,2 +1,13 @@
-package com.andybzg;public class DispatcherApplication {
+package com.andybzg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DispatcherApplication.class, args);
+    }
+
 }
